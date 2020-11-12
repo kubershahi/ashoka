19254f709d649a99ef5cb732867789ec77114466
kubershahi <kuber.shahi_ug21@ashoka.edu.in>
2020-11-12 11:16:47 +0530

    Modified files

---
+++ IML/project/script.py
@@ -0,0 +1,21 @@
+import shutil, os
+
+file = open('fold_frontal_0_data.txt', 'r')
+
+male = 0 
+female = 0
+for line in file:
+	values = line.split()
+	img = 'faces/' + values[0] +'/'+ 'coarse_tilt_aligned_face.' + values[2] + '.' + values[1]
+
+	if values[4] == "f":
+		female+=1
+		shutil.copy(img, 'Data/Gender/f')
+	elif values[4] == "m":
+		male+=1
+		shutil.copy(img, 'Data/Gender/m')
+
+print(male)
+print(female)
+
+	
