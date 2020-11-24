4be617e60d1ac7a844b8a96840841a4aac5ff51b
Kuber Shahi <kuber.shahi_ug21@ashoka.edu.in>
2020-11-24 01:45:27 -0600

    test script

--- IML/project/testscript.py
+++ IML/project/testscript.py
@@ -11,11 +11,11 @@ for line in fread:
 	values = line.split()
 
 	if (path.exists('Data/testgender/m/'+values[0])):
-		data = values[0]+' '+values[1]+' 0'
+		data = values[0]+' '+values[1]+' 1\n'
 		fwrite.write(data)
 		male+=1
 	elif(path.exists('Data/testgender/f/'+values[0])):
-		data = values[0]+' '+values[1]+' 1'
+		data = values[0]+' '+values[1]+' 0\n'
 		fwrite.write(data)
 		female+=1
 
